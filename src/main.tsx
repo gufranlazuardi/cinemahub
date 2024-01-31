dcb79e3c45133e99b54ef557805aa455c434172b
gufranlazuardi <gufranlazuardi@gmail.com>
2024-01-31 22:17:35 +0700

    feat: main pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/index.tsx";
+import App from "./routes/index";
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
