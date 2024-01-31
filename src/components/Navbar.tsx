8d6b11a965e43188bb4c6bb2585f78bcdf6e1310
gufranlazuardi <gufranlazuardi@gmail.com>
2024-01-31 22:17:55 +0700

    feat: navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Search } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-20 flex items-center pb-8">
+      <div className="flex justify-between w-full">
+        <h1 className="text-3xl text-green-600 font-bold">Cinemahub</h1>
+        <div className="w-1/3 h-10 px-4 flex gap-4 border rounded-md items-center justify-between">
+          <p className=" text-xs">Search...</p>
+          <Search size={18} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
