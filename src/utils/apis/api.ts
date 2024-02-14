46f85fb96e4da7202655769074d589ab020ea05c
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-14 19:12:15 +0700

    fix: api

--- src/utils/apis/api.ts
+++ src/utils/apis/api.ts
@@ -1,10 +1,39 @@
 import axiosWithConfig from "../axiosWithConfig";
 
-export const getAllMovies = async () => {
+export const getPopularList = async () => {
   try {
-    const response = await axiosWithConfig.get(`/movie/changes/`);
+    const response = await axiosWithConfig.get(`/movie/popular`);
 
-    return response.data as Response;
+    return response.data;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
+
+export const getNowPlayingList = async () => {
+  try {
+    const response = await axiosWithConfig.get(`/movie/now_playing`);
+
+    return response.data;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
+export const getTopRatedList = async () => {
+  try {
+    const response = await axiosWithConfig.get(`/movie/top_rated`);
+
+    return response.data;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
+
+export const getUpcomingList = async () => {
+  try {
+    const response = await axiosWithConfig.get(`/movie/upcoming`);
+
+    return response.data;
   } catch (error: any) {
     throw Error(error.response.data.message);
   }
@@ -13,16 +42,6 @@ export const getAllMovies = async () => {
 export const getDetailMovie = async (movie_id: number) => {
   try {
     const response = await axiosWithConfig.get(`/movie/${movie_id}`);
-
-    return response.data;
-  } catch (error: any) {
-    throw Error(error.response.data.message);
-  }
-};
-
-export const getListMovie = async () => {
-  try {
-    const response = await axiosWithConfig.get(`/movie/now_playing`);
 
     return response.data;
   } catch (error: any) {
