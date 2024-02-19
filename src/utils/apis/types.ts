543e3ae230667bf4969ca086406acfbf594ba076
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-20 00:54:17 +0700

    refactor: types

--- src/utils/apis/types.ts
+++ src/utils/apis/types.ts
@@ -25,26 +25,37 @@ export interface MovieDetail {
 }
 
 export interface MovieList {
-  dates: {
-    maximum: string;
-    minimum: string;
-  };
   page: number;
-  result: {
-    adult: boolean;
-    backdrop_path: string;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    poster_path: string;
-    release_date: string;
-    title: string;
-    video: boolean;
-    vote_average: string;
-    vote_count: string;
-  };
+  results: MovieItem[];
   total_pages: number;
   total_results: number;
+}
+
+export interface MovieItem {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: string;
+  vote_count: string;
+}
+
+export interface MovieVideoResult {
+  iso_639_1: string;
+  iso_3166_1: string;
+  name: string;
+  key: string;
+  site: string;
+  size: number;
+  type: string;
+  official: boolean;
+  published_at: string;
+  id: string;
 }
