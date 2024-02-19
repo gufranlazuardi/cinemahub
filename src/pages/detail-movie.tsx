7590c2055dd54e5e27f7b939bd56e99baa1c6995
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-20 00:52:52 +0700

    refactor: detail movie

--- src/pages/detail-movie.tsx
+++ src/pages/detail-movie.tsx
@@ -1,31 +1,60 @@
 import Layout from "@/components/Layout";
+import { useToast } from "@/components/ui/use-toast";
+import { getDetailMovie } from "@/utils/apis";
+import { MovieDetail } from "@/utils/apis/types";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailMovie = () => {
+  const { toast } = useToast();
+  const [detail, setDetail] = useState<MovieDetail>();
+  const { movie_id } = useParams();
+
+  console.log("params");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    try {
+      const result = await getDetailMovie(parseInt(movie_id ?? ""));
+      // const videoDetail = await
+      setDetail(result);
+    } catch (error: any) {
+      toast({
+        title: "Oops! Something went wrong.",
+        description: error.toString(),
+        variant: "destructive",
+      });
+    }
+  }
+
   return (
     <Layout>
       <div className="flex flex-col gap-6">
         <img
-          src="https://pbs.twimg.com/media/ESIJQ9IXYAIssnE?format=jpg&name=large"
-          alt="no-time-to-die"
-          className=" w-full h-96"
+          src={`https://image.tmdb.org/t/p/w500/${detail?.backdrop_path}`}
+          alt={detail?.original_title}
+          className="w-full h-96 bg-cover object-cover object-top"
         />
 
         <div className="flex gap-7">
           <div className="flex">
             <img
-              src="https://pbs.twimg.com/media/ESIJQ9IXYAIssnE?format=jpg&name=large"
-              alt="no-time-to-die"
-              className=" w-36 h-auto"
+              src={`https://image.tmdb.org/t/p/w500/${detail?.poster_path}`}
+              alt={detail?.original_title}
+              className="w-36 h-auto"
             />
           </div>
           <div className="flex flex-col gap-2">
-            <p className="text-2xl font-bold">No Time to Die (2021)</p>
+            <p className="text-2xl font-bold">{detail?.original_title}</p>
             <p className=" text-md">
-              The mission that changes everything begins…
+              {detail?.tagline}
+              {/* deskripsi film */}
             </p>
             <div>
-              <p>29 Sep 2021</p>
-              <p>163 Min</p>
+              <p>{detail?.release_date}</p>
+              <p>{detail?.runtime}</p>
             </div>
             <p className=" text-7xl">9.4</p>
             <div className="flex gap-2 pt-1">
