165fcf0347dc08516f3339c942c633428fee0bd5
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-20 00:52:13 +0700

    feat: movie card on list

---
+++ src/components/MovieCardOnList.tsx
@@ -0,0 +1,34 @@
+import { MovieItem } from "@/utils/apis/types";
+import { useNavigate } from "react-router-dom";
+
+type Props = {
+  data: MovieItem;
+};
+
+const MovieCardOnList = (props: Props) => {
+  const { data } = props;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <div
+        className="flex flex-col gap-2 cursor-pointer"
+        onClick={() => {
+          navigate(`/detail-movie/${data.id}`);
+        }}
+      >
+        <img
+          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          alt={data.title}
+          className="w-auto h-80 border-black rounded-xl"
+        />
+        <div>
+          <p className="font-bold text-md">{data.title}</p>
+          <p className="text-sm">{new Date(data.release_date).getFullYear()}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MovieCardOnList;
