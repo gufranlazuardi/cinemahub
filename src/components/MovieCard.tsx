148806aeb1e6886ae7df3e436c833eb7599731fc
gufranlazuardi <gufranlazuardi@gmail.com>
2024-02-20 00:51:59 +0700

    feat: movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,29 +1,30 @@
-import { MovieList } from "@/utils/apis/types";
+import { MovieItem } from "@/utils/apis/types";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
-  data: MovieList;
+  data: MovieItem;
 };
 
 const MovieCard = (props: Props) => {
   const { data } = props;
   const navigate = useNavigate();
+
   return (
     <>
       <div
         className="flex flex-col gap-2 cursor-pointer"
         onClick={() => {
-          navigate("/detail-movie");
+          navigate(`/detail-movie/${data.id}`);
         }}
       >
         <img
-          src={data.result.poster_path}
-          alt={data.result.original_title}
+          src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          alt={data.original_title}
           className="w-auto h-80 border-black rounded-xl"
         />
         <div>
-          <p className="font-bold txt-md">{data.result.original_title}</p>
-          <p className=" text-sm">{data.result.genre_ids}</p>
+          <p className="font-bold txt-md">{data.title}</p>
+          <p className="text-sm">{new Date(data.release_date).getFullYear()}</p>
         </div>
       </div>
     </>
