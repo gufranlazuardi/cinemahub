3393b1410551e581edaf126ecd91b55d95507e23
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-05 23:27:27 +0700

    refactor: list-now-playing page

--- src/pages/list-now-playing.tsx
+++ src/pages/list-now-playing.tsx
@@ -1,27 +1,29 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
 import MovieCardOnList from "@/components/MovieCardOnList";
+import PaginationBar from "@/components/PaginationBar";
 import { useToast } from "@/components/ui/use-toast";
+import { CornerUpLeft } from "lucide-react";
 import Layout from "@/components/Layout";
 
 import { getNowPlayingList, MovieItem } from "@/utils/apis/index";
 
-import { CornerUpLeft } from "lucide-react";
-
-const ListNowPlaying = () => {
-  const [dataNowPlayings, setDataNowPlayings] = useState<MovieItem[]>([]);
+const ListPopular = () => {
+  const [dataNowPlaying, setDataNowPlaying] = useState<MovieItem[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
   const { toast } = useToast();
+  const pageSize = 12;
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [currentPage]);
 
   async function fetchData() {
     try {
       const result = await getNowPlayingList();
-      setDataNowPlayings(result.results);
+      setDataNowPlaying(result.results);
     } catch (error: any) {
       toast({
         title: "Oops! Something went wrong.",
@@ -30,6 +32,19 @@ const ListNowPlaying = () => {
       });
     }
   }
+
+  const handlePageChange = (direction: "prev" | "next") => {
+    setCurrentPage((prevPage) =>
+      direction === "prev" ? prevPage - 1 : prevPage + 1
+    );
+  };
+
+  const totalPages = Math.ceil(dataNowPlaying.length / pageSize);
+
+  const visibleMovies = dataNowPlaying.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
 
   return (
     <Layout>
@@ -46,17 +61,23 @@ const ListNowPlaying = () => {
           </div>
           <h1 className="text-4xl font-bold py-2 mx-auto">Now Playing</h1>
         </div>
-        <div className=" border-l-8 border-lime-500 pl-2">
+        <div className="border-l-8 border-lime-500 pl-2">
           <p className="text-lg font-semibold">Recently Added</p>
         </div>
         <div className="py-5 gap-6 grid grid-cols-6">
-          {dataNowPlayings.map((nowPlaying, index) => (
+          {visibleMovies.map((nowPlaying, index) => (
             <MovieCardOnList data={nowPlaying} key={index} />
           ))}
         </div>
+
+        <PaginationBar
+          currentPage={currentPage}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+        />
       </div>
     </Layout>
   );
 };
 
-export default ListNowPlaying;
+export default ListPopular;
