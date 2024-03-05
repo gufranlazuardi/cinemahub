7f9f6b208c13f3b7bc077adeaa1074a7df65ec2c
gufranlazuardi <gufranlazuardi@gmail.com>
2024-03-05 22:57:41 +0700

    feat: pagination

--- src/components/PaginationBar.tsx
+++ src/components/PaginationBar.tsx
@@ -1,31 +1,40 @@
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import React from "react";
 
-const PaginationBar = () => {
+interface PaginationBarProps {
+  currentPage: number;
+  totalPages: number;
+  handlePageChange: (direction: "prev" | "next") => void;
+}
+
+const PaginationBar: React.FC<PaginationBarProps> = ({
+  currentPage,
+  totalPages,
+  handlePageChange,
+}) => {
   return (
-    <Pagination>
-      <PaginationContent>
-        <PaginationItem>
-          <PaginationPrevious href="#" />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationEllipsis />
-        </PaginationItem>
-        <PaginationItem>
-          <PaginationNext href="#" />
-        </PaginationItem>
-      </PaginationContent>
-    </Pagination>
+    <div className="flex justify-center mt-8 gap-6">
+      <button
+        onClick={() => handlePageChange("prev")}
+        disabled={currentPage === 1}
+        className="hover:bg-gray-200 transition-colors duration-300 px-2 py-2 rounded-md"
+      >
+        <div className="flex justify-between gap-2">
+          <ChevronLeft />
+          <p>Previous</p>
+        </div>
+      </button>
+      <button
+        onClick={() => handlePageChange("next")}
+        disabled={currentPage === totalPages}
+        className="hover:bg-gray-200 transition-colors duration-300 px-2 py-2 rounded-md"
+      >
+        <div className="flex justify-between gap-2">
+          <p>Next</p>
+          <ChevronRight />
+        </div>
+      </button>
+    </div>
   );
 };
 
